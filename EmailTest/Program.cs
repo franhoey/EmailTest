f5bbdd78c1dee825e86383f2832f4cd64a703c1c
fran.hoey <fran@bluejumper.com>
2014-11-06 09:42:47 +0000

    Added program files

---
+++ EmailTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var message = new MailMessage()
+                {
+                    Subject = ConfigurationManager.AppSettings["Subject"],
+                    Body = ConfigurationManager.AppSettings["Body"]
+                };
+                message.To.Add(ConfigurationManager.AppSettings["ToAddress"]);
+
+                var smtp = new SmtpClient();
+                smtp.Send(message);
+
+                Console.WriteLine("Email Sent.");
+            }
+            catch (Exception ex)
+            {
+                var outputException = ex;
+                while (outputException != null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(outputException.Message);
+                    Console.WriteLine(outputException.StackTrace);
+
+                    outputException = outputException.InnerException;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close.");
+
+            Console.ReadKey();
+        }
+    }
+}
